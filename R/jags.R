18ed5d5900b6798e2be3041897abb60bf29bf1f6
martyn_plummer <devnull@localhost>
2008-11-02 18:07:01 +0000

    Allow jags.samples and coda.samples to monitor subsets of node arrays.

--- R/jags.R
+++ R/jags.R
@@ -242,6 +242,73 @@ jags.model <- function(file, data=sys.frame(sys.parent()), inits,
     return(model)
 }
 
+parse.varname <- function(varname) {
+
+  ## Try to parse string of form "a" or "a[n,p:q,r]" where "a" is a
+  ## variable name and n,p,q,r are integers
+
+  v <- try(parse(text=varname, n=1), silent=TRUE)
+  if (!is.expression(v) || length(v) != 1)
+    return(NULL)
+
+  v <- v[[1]]
+  if (is.name(v)) {
+    ##Full node array requested
+    return(list(name=deparse(v)))
+  }
+  else if (is.call(v) && identical(deparse(v[[1]]), "[") && length(v) > 2) {
+    ##Subset requested
+    ndim <- length(v) - 2
+    lower <- upper <- numeric(ndim)
+    if (any(nchar(sapply(v, deparse)) == 0)) {
+      ##We have to catch empty indices here or they will cause trouble
+      ##below
+      return(NULL)
+    }
+    for (i in 1:ndim) {
+      index <- v[[i+2]]
+      if (is.numeric(index)) {
+        ##Single index
+        lower[i] <- upper[i] <- index
+      }
+      else if (is.call(index) && length(index) == 3 &&
+               identical(deparse(index[[1]]), ":") &&
+               is.numeric(index[[2]]) && is.numeric(index[[3]]))
+        {
+          ##Index range
+          lower[i] <- index[[2]]
+          upper[i] <- index[[3]]
+        }
+      else return(NULL)
+    }
+    if (any(upper < lower))
+      return (NULL)
+    return(list(name = deparse(v[[2]]), lower=lower, upper=upper))
+  }
+  return(NULL)
+}
+
+parse.varnames <- function(varnames)
+{
+  names <- character(length(varnames))
+  lower <- upper <- vector("list", length(varnames))
+  for (i in seq(along=varnames)) {
+    y <- parse.varname(varnames[i])
+    if (is.null(y)) {
+      stop(paste("Invalid variable subset", varnames[i]))
+    }
+    names[i] <- y$name
+    if (!is.null(y$lower)) {
+      lower[[i]] <- y$lower
+    }
+    if (!is.null(y$upper)) {
+      upper[[i]] <- y$upper
+    }
+  }
+  return(list(names=names, lower=lower, upper=upper))
+}
+
+
 jags.samples <-
   function(model, variable.names=NULL, n.iter, thin=1, type="trace")
 {
@@ -263,8 +330,9 @@ jags.samples <-
               type, PACKAGE="rjags")
     }
     else {
-        .Call("set_monitors", model$ptr(), variable.names,
-              as.integer(thin), type, PACKAGE="rjags")
+      pn <- parse.varnames(variable.names)
+      .Call("set_monitors", model$ptr(), pn$names, pn$lower, pn$upper,
+            as.integer(thin), type, PACKAGE="rjags")
     }
     update(model, n.iter)
     ans <- .Call("get_monitored_values", model$ptr(), type, PACKAGE="rjags")
@@ -276,8 +344,8 @@ jags.samples <-
     }
     else {
         for (i in seq(along=variable.names)) {
-            .Call("clear_monitor", model$ptr(), variable.names[i], type,
-                  PACKAGE="rjags")
+            .Call("clear_monitor", model$ptr(), pn$names[i], pn$lower[[i]],
+                  pn$upper[[i]], type, PACKAGE="rjags")
         }
     }
     return(ans)
